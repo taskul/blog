7507e53066eafe4190dd794519fb3ea752c7ca19
tastasterone <tastankulmeshkenov@gmail.com>
2021-06-27 19:14:50 -0500

    Update database to postgres, add psycopg2-binary to requirements.txt, change blog name in header and index html files

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentsForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 #Ckeditor
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
